8776abf58657d240fdce0678b4e9ec581d537dae
fractalbach <fractalbach@gmail.com>
2018-10-19 22:50:05 -0700

    added ability to move entities; made menu bar into title bar

--- src/game.js
+++ src/game.js
@@ -1,33 +1,26 @@
+const TILE_SIZE = 50;
+
 /* 
 _______________________________________________________________________
-                             Display
+                             Helper Functions
 =======================================================================
-Display handles:
-    - graphics
-    - interactions
-    - document objects
+Various functions that aren't methods but are useful for various
+purpoes.  Refactor these as static methods of a "toolkit" class 
+later on if needed.
+_______________________________________________________________________
 */
-class Display {
-    constructor(rows, cols) {
-        // grid: the area with all of the canvas tiles.
-        this.grid = new Grid(rows, cols, 50, 50);
 
-        // playerLayer: the transparent div where player canvases are.
-        this.playerLayer = makeLayer('playerLayer');
-
-        // Display.element is the DOM representation of the game. 
-        this.element = document.querySelector('#game');
-        this.element.appendChild(this.grid.element);
-        this.element.addEventListener('mousedown', highlightTarget);
-        this.element.addEventListener('click', highlightTarget);   
+// kudos to https://eloquentjavascript.net/ for this helper function.
+function makeElement(name, attrs) {
+    let ele = document.createElement(name);
+    if (attrs == undefined) {
+        return ele;
     }
-    Add(entity, name) {}
-
-    Move(entity) {
-        entity.target
+    for (let attr of Object.keys(attrs)) {
+        ele.setAttribute(attr, attrs[attr]);
     }
+    return ele;
 }
-
 
 function makeLayer(name) {
     let ele = document.createElement('div');
@@ -41,18 +34,113 @@ function highlightTarget(event) {
 }
 
 
-class Grid {
-    constructor(nRows, nCols, width, height) {
+let uid = 123;
+function nextUid() {
+    uid++;
+    return uid;
+}
+
+/* 
+_______________________________________________________________________
+                             Display
+=======================================================================
+Display handles:
+    - graphics
+    - interactions
+    - document objects
+_______________________________________________________________________
+
+*/
+
+
+class Display 
+{
+    constructor(rows, cols) {
+        // grid: the area with all of the canvas tiles.
+        this.grid = new Grid(rows, cols, TILE_SIZE, TILE_SIZE);
+
+        // playerLayer: the transparent div where player canvases are.
+        this.playerLayer = makeLayer('playerLayer');
+
+        // Display.element is the DOM representation of the game. 
+        this.element = document.querySelector('#game');
+
+        // A collection of general-purpose canvas elements.
+        this.entities = new Map();
+
+        // Call init procedure.
+        this.init();
+    }
+
+    init() {
+        // Add the grid to the game area. 
+
+        this.element.appendChild(this.grid.element);
+        this.element.appendChild(this.playerLayer);
+
+
+        // Add global event listeners
+        this.element.addEventListener('mousedown', highlightTarget);
+        this.element.addEventListener('click', highlightTarget);
+
+        // Add the display element to the game.
+        // document.body.appendChild(this.element);
+    }
+
+    add(key, element) {
+        this.entities.set(key, element);
+    }
+
+    getEle(key) {
+        return this.entities.get(key);
+    }
+
+    addPlayer(key) {
+        let attrs = {
+            id: `player${nextUid()}`,
+            class: 'player',
+            width: TILE_SIZE,
+            height: TILE_SIZE,
+        };
+        let e = makeElement('canvas', attrs);
+        this.add(key, e);
+        this.playerLayer.appendChild(e);
+    }
+
+    setLocation(key, location) {
+        if (!this.entities.has(key)) {
+            console.warn(`Display.setLocation: The key '${key.toString()}' hasn't been registered.`);
+            return;
+        }
+        let e = this.entities.get(key);
+        let pixel = this.LocToPixel(location);
+        e.style.left = pixel.px;
+        e.style.top = pixel.py;
+        console.log(`location of ${key.toString()} set to: (left, top) = `, pixel)
+    }
+
+    LocToPixel(location) {
+        let tile = this.grid.tileAt(location);
+        return new PixelPoint(tile.offsetLeft, tile.offsetTop)
+    }
+}
+
+class Grid 
+{
+    constructor(nRows, nCols, width, height) 
+    {
         this.matrix = new Array(nRows);
         this.element = makeLayer('groundLayer');
 
-        for (let row = 0; row < nRows; row++) {
+        for (let row = 0; row < nRows; row++) 
+        {
             this.matrix[row] = new Array(nCols);
             let rowElement = document.createElement('div');
             rowElement.classList.add('tileRow');
             this.element.append(rowElement);
 
-            for (let col = 0; col < nCols; col++) {
+            for (let col = 0; col < nCols; col++) 
+            {
                 let ele = document.createElement('canvas');
                 ele.width = width;
                 ele.height = height;
@@ -62,32 +150,41 @@ class Grid {
             }
         }
     }
+
     tile(row, col) {
         return this.matrix[row][col];
     }
-}
 
-class PlayerCanvas {
-    constructor(width=20, height=20) {
-        this.canvas = document.createElement('canvas');
-        this.canvas.className = 'player';
-        this.canvas.width = width;
-        this.canvas.height = height;
+    tileAt(location) {
+        return this.matrix[location.row][location.col];
     }
 }
 
-// Point is a pixel location literal. 
-class Point {
+
+
+// PixelPoint represents an (x,y) pixel offset from the top-left of screen.
+class PixelPoint {
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
-    Add(pixelLocation) {
+    AdjustBy(pixelPoint) {
         this.x += pixelLocation.x;
         this.y += pixelLocation.y;
     }
     toString() {
-        return `(${this.x}px, ${this.y}px)`
+        return `[${this.x}px, ${this.y}px]`
+    }
+    get px() {
+        return `${this.x}px`
+    }
+    get py() {
+        return `${this.y}px`
+    }
+    static Add(pixelPoint1, pixelPoint2) {
+        let x = pixelPoint1.x + pixelPoint2.x;
+        let y =  pixelPoint1.y + pixelPoint2.y;
+        return new PixelPoint(x,y);
     }
 }
 
@@ -103,17 +200,44 @@ State handles:
     - game logic
     - timers
     - keeps track of actual player positions.
+APIS:
+    - todo
+    -
+_______________________________________________________________________
 */
+
 class State {
     constructor() {
+        this.data = {};
+        this.serverData = {};
         this.players = new Map();
+        this.projectiles = [];
+    }
+    merge(newData) {
+		Object.assign(this.data, newData);
+    }
+    mergeWithServerData() {
+        Object.assign(this.data, this.serverData);
+    }
+    addPlayer(username) {
+        this.players.set(username, new Player())
     }
 }
 
 
+
+
+
+
+/* 
+_______________________________________________________________________
+                Nouns:    People, Places, and Things
+=======================================================================
+*/ 
+
 // A location is a logical location.
 class Location {
-    constructor(row=-1, col=-1, z=-1) {
+    constructor(row=-1, col=-1) {
         this.row = row;
         this.col = col;
     }
@@ -126,6 +250,7 @@ class Entity {
         this.targetLocation = new Location(-1, -1);
     }
 }
+
 class Player extends Entity {
     constructor(username) {
         super(Entity);
@@ -136,18 +261,26 @@ class Player extends Entity {
 
 
 
-
-
-
  
 
 /* 
 _______________________________________________________________________
                              Class Game
 =======================================================================
-Class Game unifies the Display and the State by bridiging 
-the logic  with the visuals.
+Main purposes of the Game class:
+    1. unify game state & user interface.
+    2. provide sensible APIs for human consumption.
 
+
+APIs 
+------------------------------------------------
+Done:
+    - addPlayer
+    - movePlayer
+
+TODO: 
+    - removePlayer
+    -   
 
 
 Information Travel:
@@ -159,12 +292,72 @@ Information Travel:
         |
      [State]  <---[Timers or Network Data ]
 
-    
-
+    class PlayerCanvas {
+    constructor(width=20, height=20) {
+        this.canvas = document.createElement('canvas');
+        this.canvas.className = 'player';
+        this.canvas.width = width;
+        this.canvas.height = height;
+    }
+}
+_______________________________________________________________________
 */ 
+
 export class Game {
     constructor() {
         this.display = new Display(30, 30);
         this.state = new State();
+        startGame(this);
     }
+
+    addPlayer(key) {
+        this.state.addPlayer(key);
+        this.display.addPlayer(key);
+    }
+
+    getPlayerElement(key) {
+        return this.display.getEle(key);
+    }
+
+    movePlayer(key, row, col) {
+        let loc = new Location(row, col);
+        let p = this.state.players.get(key);
+        p.targetLocation = loc;
+        this.display.setLocation(key, loc);
+    }
+
 };
+
+// startGame is a private function that launches the game for the first time.
+// it happens automatically after the Game object is created.
+let hasStarted = false;
+function startGame(game) {
+
+    // makes sure that this function doesn't run twice.
+    if (hasStarted) {
+        return;
+    }
+
+    test(game);
+    
+    // declare that the game has started.
+    hasStarted = true;
+}
+
+
+// -------------------------------------------------
+// BEGIN testing area.
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function test(game) {
+
+    // add a player to the game map.
+    let me = Symbol('me');
+    game.addPlayer(me);
+    game.movePlayer(me, 1, 2);
+
+    console.log(game.display.entities)
+
+}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// END testing area.
+// -------------------------------------------------
