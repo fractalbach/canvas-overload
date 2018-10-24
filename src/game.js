9a19465a9e15ab3f2b63ba91bc383fb786ce6dce
fractalbach <fractalbach@gmail.com>
2018-10-23 21:41:32 -0700

    major changes to codebasegit status!  added smooth movement.

--- src/game.js
+++ src/game.js
@@ -1,45 +1,35 @@
-const TILE_SIZE = 50;
-
 /* 
 _______________________________________________________________________
-                             Helper Functions
+                        Constants and Variables
 =======================================================================
-Various functions that aren't methods but are useful for various
-purpoes.  Refactor these as static methods of a "toolkit" class 
-later on if needed.
+Stuff that is easiest when its in a global scope.
 _______________________________________________________________________
 */
 
-// kudos to https://eloquentjavascript.net/ for this helper function.
-function makeElement(name, attrs) {
-    let ele = document.createElement(name);
-    if (attrs == undefined) {
+const TILE_SIZE = 50;
+const ME = "myplayer";
+var GAME;
+
+/* 
+_______________________________________________________________________
+                             Utility Functions
+=======================================================================
+Extra functions that are useful for a variety of cases.
+_______________________________________________________________________
+*/
+
+class util {
+    static makeElement(name, attrs) {
+        let ele = document.createElement(name);
+        if (attrs == undefined) {
+            return ele;
+        }
+        for (let attr of Object.keys(attrs)) {
+            ele.setAttribute(attr, attrs[attr]);
+        }
         return ele;
     }
-    for (let attr of Object.keys(attrs)) {
-        ele.setAttribute(attr, attrs[attr]);
-    }
-    return ele;
 }
-
-function makeLayer(name) {
-    let ele = document.createElement('div');
-    ele.classList.add('layer');
-    ele.id = name;
-    return ele;
-}
-
-function highlightTarget(event) {
-    event.target.classList.add('highlighted');
-}
-
-let uid = 123;
-function nextUid() {
-    uid++;
-    return uid;
-}
-
-let resizeTimer;
 
 /* 
 _______________________________________________________________________
@@ -53,135 +43,72 @@ _______________________________________________________________________
 
 */
 
-
-class Display 
-{
+class Display {
     constructor(rows, cols) {
+
         // grid: the area with all of the canvas tiles.
         this.grid = new Grid(rows, cols, TILE_SIZE, TILE_SIZE);
 
-        // playerLayer: the transparent div where player canvases are.
-        this.playerLayer = makeLayer('playerLayer');
-
         // Display.element is the DOM representation of the game. 
-        this.element = document.querySelector('#game');
+        this.element = document.querySelector('#Display');
+
+        // entLayer: the transparent div where entity canvases are.
+        this.entLayer = document.querySelector('#DisplayEnts')
 
         // A collection of entities (aka ents).
         this.ents = new Map();
 
-        // Call init procedure.
-        this.init();
-    }
-
-    init() {
-        // Add the grid to the game area. 
-        this.element.appendChild(this.grid.element);
-        this.element.appendChild(this.playerLayer);
-
-
-        // Interactions with the Display
-        this.element.addEventListener('mousedown', highlightTarget);
-        this.element.addEventListener('click', highlightTarget);
 
         // Auto-adjust object positions whenever screen is resized.
-        window.addEventListener('resize', ()=> this.refreshPositions());
-
-        // Add the display element to the game.
-        // document.body.appendChild(this.element);
+        // window.addEventListener('resize', ()=> this.refreshPositions());
     }
 
-    refreshPositions() {
-        for (let e of this.ents.values()) {
-            this.updatePosition(e);
-        }
+    add(key, entity) {
+        this.ents.set(key, entity);
+        this.entLayer.appendChild(entity.element);
+        entity.grid = this.grid;
+        entity.alignWithGrid();
     }
 
-    add(key, element) {
-        this.ents.set(key, element);
-    }
-
-    getEle(key) {
-        return this.ents.get(key);
-    }
-
-    addPlayer(key, entity) {
-        let attrs = {
-            id: `player${nextUid()}`,
-            class: 'player',
-            width: TILE_SIZE,
-            height: TILE_SIZE,
-        }
-        let element = makeElement('canvas', attrs);
-        let ent = new DisplayEntity(entity, element);
-        this.add(key, ent);
-        this.updatePosition(ent);
-        this.playerLayer.appendChild(element);
-    }
-
-    setLocation(key, location) {
-        if (!this.ents.has(key)) {
-            console.warn(`Display.setLocation: The key '${key.toString()}' hasn't been registered.`);
-            return;
-        }
-        let e = this.ents.get(key);
-        e.location = location;
-        this.updatePosition(e);
-    }
-
-    LocToPixel(location) {
-        let tile = this.grid.tileAt(location);
-        return new PixelPoint(tile.offsetLeft, tile.offsetTop);
-    }
-
-    updatePosition(displayEntity) {
-        let pixel = this.LocToPixel(displayEntity.location);
-        displayEntity.element.style.left = pixel.px;
-        displayEntity.element.style.top = pixel.py;
-    }
-
-}
-
-// combines the logical location of an Entity with its DOM element.
-class DisplayEntity {
-    constructor(entity, element) {
-        this.entity = entity;
-        this.element = element;
-    }
-
-    get location() {
-        return this.entity.location;
-    }
-
-    set location(location) {
-        this.entity.location = location;
+    highlightTarget(event) {
+        event.target.classList.add('highlighted');
+        let [row, col] = this.grid.locationOfTile(event.target);
+        console.log('Clicked:', event.target, `  Location:(${row}, ${col}))`);
     }
 }
 
 
 
+class Grid {
+    constructor(nRows, nCols, width, height) {
 
-class Grid 
-{
-    constructor(nRows, nCols, width, height) 
-    {
+        // matrix is an array of rows, where each element is an element.
         this.matrix = new Array(nRows);
-        this.element = makeLayer('groundLayer');
 
+        // reverseTileMap (Element --> Location).  Maps tile elements to their logical locations.
+        this.reverseTileMap = new Map();
+
+        // Grid layer
+        this.element = document.querySelector('#DisplayGrid');
+
+
+        // Initialize the matrix and create each of the tiles.
         for (let row = 0; row < nRows; row++) 
         {
             this.matrix[row] = new Array(nCols);
-            let rowElement = document.createElement('div');
-            rowElement.classList.add('tileRow');
+            let rowElement = util.makeElement('div', {class: 'tileRow'});
             this.element.append(rowElement);
 
             for (let col = 0; col < nCols; col++) 
             {
-                let ele = document.createElement('canvas');
-                ele.width = width;
-                ele.height = height;
-                ele.classList.add('tile');
-                rowElement.append(ele);
-                this.matrix[row][col] = ele;
+                let tile = util.makeElement('canvas', {
+                    width: width,
+                    height: height,
+                    class: 'tile'
+                });
+                rowElement.append(tile);
+                this.matrix[row][col] = tile;
+                this.reverseTileMap.set(tile, [row, col]);
             }
         }
     }
@@ -190,39 +117,15 @@ class Grid
         return this.matrix[row][col];
     }
 
-    tileAt(location) {
-        return this.matrix[location.row][location.col];
+    locationOfTile(element) {
+        return this.reverseTileMap.get(element);
+    }
+
+    locationToXY(row, col) {
+        let tile = this.tile(row, col);
+        return [tile.offsetLeft, tile.offsetTop];
     }
 }
-
-
-
-// PixelPoint represents an (x,y) pixel offset from the top-left of screen.
-class PixelPoint {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    AdjustBy(pixelPoint) {
-        this.x += pixelLocation.x;
-        this.y += pixelLocation.y;
-    }
-    toString() {
-        return `[${this.x}px, ${this.y}px]`
-    }
-    get px() {
-        return `${this.x}px`
-    }
-    get py() {
-        return `${this.y}px`
-    }
-    static Add(pixelPoint1, pixelPoint2) {
-        let x = pixelPoint1.x + pixelPoint2.x;
-        let y =  pixelPoint1.y + pixelPoint2.y;
-        return new PixelPoint(x,y);
-    }
-}
-
 
 /* 
 _______________________________________________________________________
@@ -230,26 +133,111 @@ _______________________________________________________________________
 =======================================================================
 */ 
 
-// A location is a logical location.
-class Location {
-    constructor(row=0, col=0) {
-        this.row = row;
-        this.col = col;
+const areArrsEqual = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return false;
     }
+    for (let i=0; i<arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
 }
+
+const getNextLocation = (startRow, startCol, endRow, endCol) => {
+        let row = startRow;
+        let col = startCol;
+
+        if (startRow < endRow) {
+            row++;
+        } else if (startRow > endRow) {
+            row--;
+        }
+        
+        if (startCol < endCol) {
+            col++;
+        } else if (startCol > endCol) {
+            col--;
+        }
+        return [row, col];
+    }
+
+
 
 // An entity is a person or thing that has a location in the world.
 class Entity {
     constructor(element, row=0, col=0) {
-        this.location = new Location(row, col);
-        this.targetLocation = new Location(row, col);
+        this.element = element;
+        this.grid = undefined;
+
+        this.currentRow = row;
+        this.currentCol = col;
+        
+        this.targetRow = row;
+        this.targetCol = col;
+
+        this.nextRow = row;
+        this.nextCol = col;
+
+        this.pace = 300;
+        this.lock = false;
+
     }
+
+    alignWith(element) {
+        this.element.style.top = `${element.offsetTop}px`;
+        this.element.style.left = `${element.offsetLeft}px`;
+    }
+
+    alignWithGrid() {
+        if (this.grid == undefined) {
+            console.warn(this, "alignWithGrid: entity doesn't have a reference to grid.");
+            return;
+        }
+        let tile = this.grid.tile(this.currentRow, this.currentCol);
+        this.alignWith(tile);
+    }
+
+    alignWithPixel(x, y) {
+        this.element.style.left = `${x}px`;
+        this.element.style.top = `${y}px`;
+    }
+
+    isAtTarget() {
+        return ((this.currentRow === this.targetRow) && (this.currentCol === this.targetCol));
+    }
+
+
+    updateFrame() {
+        if (this.lock) {
+            // console.warn('locked.', this);
+            return;
+        }
+        if (this.isAtTarget()) {
+            return;
+        }
+        [this.nextRow, this.nextCol] = getNextLocation(this.currentRow, this.currentCol, this.targetRow, this.targetCol);
+        // console.warn(`Next Location = (${this.nextRow}, ${this.nextCol})`)
+        
+        let [x0, y0] = this.grid.locationToXY(this.currentRow, this.currentCol);
+        let [xf, yf] = this.grid.locationToXY(this.nextRow, this.nextCol);
+        let pace = this.pace;
+        let ent = this;
+        startFrameLoop(x0, y0, xf, yf, ent);
+    }
+
 }
 
 class Player extends Entity {
-    constructor(username) {
+    constructor(key) {
         super(Entity);
-        this.username = username;
+        this.element = util.makeElement('canvas', {
+            id: `player${key}`,
+            class: 'player',
+            width: TILE_SIZE,
+            height: TILE_SIZE,
+        });
     }
 }
 
@@ -298,19 +286,8 @@ Main purposes of the Game class:
     1. unify game state & user interface.
     2. provide sensible APIs for human consumption.
 
-
-APIs 
 ------------------------------------------------
-Done:
-    - addPlayer
-    - movePlayer
-
-TODO: 
-    - removePlayer
-    -   
-
-
-Information Travel:
+Information Travel
 ------------------------------------------------
 
      [Display]  <---[User Interactions]
@@ -319,14 +296,6 @@ Information Travel:
         |
      [State]  <---[Timers or Network Data ]
 
-    class PlayerCanvas {
-    constructor(width=20, height=20) {
-        this.canvas = document.createElement('canvas');
-        this.canvas.className = 'player';
-        this.canvas.width = width;
-        this.canvas.height = height;
-    }
-}
 _______________________________________________________________________
 */ 
 
@@ -338,22 +307,32 @@ export class Game {
     }
 
     addPlayer(key) {
-        let p = new Player();
+        let p = new Player(key);
         this.state.players.set(key, p);
-        this.display.addPlayer(key, p);
+        this.display.add(key, p);
     }
 
     getPlayerElement(key) {
         return this.display.getEle(key);
     }
 
-    movePlayer(key, row, col) {
-        let loc = new Location(row, col);
-        let p = this.state.players.get(key);
-        p.targetLocation = loc;
-        this.display.setLocation(key, loc);
+    setLocation(key, row, col) {
+        if (!this.state.players.has(key)) {
+            console.warn(`setLocation: entity "${key}" was not found.`);
+            return;
+        }
+        let ent = this.state.players.get(key);
+        ent.currentRow = ent.targetRow = ent.nextRow = row;
+        ent.currentCol = ent.targetCol = ent.nextCol = col;
+        ent.alignWithGrid();
     }
 
+    setTarget(key, row, col) {
+        let ent = this.state.players.get(key);
+        ent.targetRow = row;
+        ent.targetCol = col;
+        ent.updateFrame();
+    }
 };
 
 // startGame is a private function that launches the game for the first time.
@@ -373,19 +352,68 @@ function startGame(game) {
 }
 
 
-// -------------------------------------------------
-// BEGIN testing area.
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+/* 
+_______________________________________________________________________
+                             Testing Area ! 
+=======================================================================
+*/ 
 function test(game) {
+    GAME = game;
 
     // add a player to the game map.
-    let me = Symbol('me');
-    game.addPlayer(me);
-    game.movePlayer(me, 1, 2);
+    game.addPlayer(ME);
+    game.setLocation(ME, 1, 2);
+    game.setTarget(ME, 1, 3);
+    
 
-    console.log(game.display.entities);
+    let doHighlight = function(event) {
+        if (!event.target.classList.contains('tile')) {
+            return;
+        }
+        let [row, col] = game.display.grid.locationOfTile(event.target);
+        // console.log('Clicked:', event.target, `location:(${row}, ${col})`);
+        game.setTarget(ME, row, col);
+    }
+    
+    console.log(game.display.ents);
+    game.display.element.addEventListener('mousedown', doHighlight);
 
 }
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-// END testing area.
-// -------------------------------------------------
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+END testing area.
+_______________________________________________________________________
+*/
+
+
+
+
+const startFrameLoop = (x0, y0, xf, yf, ent) => {
+    let t0 = performance.now();
+    let tf = t0 + ent.pace;
+    let counter = 0;
+    ent.lock = true;
+
+    function frameStep(timestamp) {
+        counter++;
+        let t = timestamp - t0;
+        if (tf < timestamp) {
+            // console.warn(`Frame Loop Complete! Number of Steps: ${counter}`, ent);
+            ent.currentRow = ent.nextRow;
+            ent.currentCol = ent.nextCol;
+            ent.alignWithGrid();
+            ent.lock = false;
+            ent.updateFrame();
+            return;
+        }
+        let x = ((xf - x0) / (tf - t0))*t + x0;
+        let y = ((yf - y0) / (tf - t0))*t + y0;
+        ent.alignWithPixel(x, y);
+        window.requestAnimationFrame(frameStep);
+    }
+
+    window.requestAnimationFrame(frameStep);
+}
